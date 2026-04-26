e97260faa8852a814ff749ccef56e66f12b5709b
Linh Tuan Nguyen <52875747+silentlie@users.noreply.github.com>
2026-04-26 20:29:30 +1000

    chore: improve deployment update script

--- up.sh
+++ up.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-git submodule update --init --recursive
-docker compose up --build
+git pull
+git submodule update --init --recursive --remote
+
+if [[ -n "$(git status --porcelain)" ]]; then
+  git add fake-store-server
+  git commit -m "chore: update fake-store-server submodule"
+  git push
+fi
+
+docker compose down
+docker compose build --no-cache
+docker compose up
